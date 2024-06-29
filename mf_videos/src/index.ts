c33c0c5e71a1c7b5aaa2d59df84e1518ea733263
Victor Castro <victor.castro34@gmail.com>
2024-06-29 17:34:55 -0300

    feat: add search-button logic

--- mf_videos/src/index.ts
+++ mf_videos/src/index.ts
@@ -1,4 +1,59 @@
-const testElement: HTMLElement | null = document.getElementById('test');
-if (testElement) {
-  testElement.innerText = 'Hello from drawer Micro-Frontend with TypeScript';
-}
+const searchInput = document.getElementById('search-input') as HTMLInputElement
+const searchButton = document.getElementById('search-button') as HTMLButtonElement
+const videoList = document.getElementById('video-list') as HTMLUListElement
+
+
+interface VideoSnippet {
+  title: string;
+  description: string;
+  thumbnails: {
+    default: { url: string };
+    medium: { url: string };
+    high: { url: string };
+  };  
+}  
+
+interface Video {
+  id: { videoId: string };
+  snippet: VideoSnippet;
+}  
+
+const searchVideos = async (): Promise<Video | undefined> => {
+  const text = searchInput.value; 
+  try {
+    const response = await fetch('http://localhost:3000/api/getVideos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text }) 
+    });
+    
+    const videos = await response.json();
+    console.log(videos);
+
+    videoList.innerHTML = '';
+
+    videos.forEach((video: Video) => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <h2>${video.snippet.title}</h2>
+        <p>${video.snippet.description}</p>
+        <img src="${video.snippet.thumbnails.default.url}" alt="${video.snippet.title}">
+      `;
+
+      videoList.appendChild(listItem);
+
+    })
+
+  } catch (error) {
+    console.log('Error:', error);
+    return undefined;
+  }
+};
+
+searchButton.addEventListener('click', async () => {
+  await searchVideos()
+});
+
+
