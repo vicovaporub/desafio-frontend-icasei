2a313a787990ea83b5509392e4532ebcc2230f2b
Victor Castro <victor.castro34@gmail.com>
2024-06-30 21:09:52 -0300

    feat: add nav buttons, styles and functionality

--- mf_drawer/src/index.ts
+++ mf_drawer/src/index.ts
@@ -1,4 +1,25 @@
-const drawerElement: HTMLElement | null = document.getElementById('drawer');
-if (drawerElement) {
-  drawerElement.innerText = 'Hello from drawer Micro-Frontend with TypeScript';
+const videosButton = document.getElementById('videos-link') as HTMLAnchorElement;
+const favsButton = document.getElementById('favs-link') as HTMLAnchorElement;
+
+videosButton!.addEventListener('click', (event) => {
+  event.preventDefault()
+  top!.window.location.href = 'http://localhost:3000/videos'
+});
+
+favsButton!.addEventListener('click', (event) => {
+  event.preventDefault()
+  top!.window.location.href = 'http://localhost:3000/favs'
+});
+
+
+const getModeParameter = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('mode');
 }
+
+window.onload = () => {
+  const mode = getModeParameter();
+  document.body.className = ''
+  document.body.classList.add(mode!);
+}
+
